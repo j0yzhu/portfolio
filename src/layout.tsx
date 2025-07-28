f47f017b905ec2f19e1a200a1dd9980afb978fc1
Joy <xiaominzhu@gmail.com>
2025-07-28 17:05:58 +1200

    nav bar

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,35 @@
-import {Outlet} from "react-router";
+import {Link, Outlet} from "react-router";
+import {ModeToggle} from "@/components/ui/mode-toggle.tsx";
+import pig from "./assets/HomePageImages/Pig.png";
+
+function NavBar() {
+    return (
+        <div className="w-full bg-background/80 backdrop-blur px-4 py-2 flex items-center justify-between border-b ">
+            <Link to={"/"}>
+                <img className="w-20 m-2" src={pig} alt="Home"/>
+            </Link>
+            <Link to={"/about"}>
+                <span className="text-2xl font-semibold">About</span>
+            </Link>
+            <Link to={"/Projects"}>
+                <span className="text-2xl font-semibold">Projects</span>
+            </Link>
+            <Link to={"/Leadership"}>
+                <span className="text-2xl font-semibold">Leadership</span>
+            </Link>
+            <Link to={"/Gallery"}>
+                <span className="text-2xl font-semibold">Gallery</span>
+            </Link>
+            <ModeToggle/>
+        </div>
+    );
+}
 
 export default function Layout() {
     return (
-        <Outlet/>
+        <>
+            <NavBar/>
+            <Outlet/>
+        </>
     );
 }
