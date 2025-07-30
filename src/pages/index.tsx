349a16a18f882047e2ff8f735c3b8c88bdd4a7a7
Joy <xiaominzhu@gmail.com>
2025-07-30 17:52:40 +1200

    image animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,21 +6,24 @@ import coke from "@/assets/HomePageImages/coke.png";
 import ghibli from "@/assets/HomePageImages/ghibli.png";
 import nyota from "@/assets/HomePageImages/nyota.png";
 import stars from "@/assets/HomePageImages/stars.png";
+import {useState} from "react";
 
 
 export default function IndexPage() {
+    const [spin, setSpin] = useState(false);
+
     return (
         <>
             <div className="relative w-full h-screen">
                 <div>
-                    <img className="absolute w-[15vw] top-[5vh] left-[5vw] object-contain" src={cake} alt="cake"/>
-                    <img className="absolute w-[17vw] top-[5vh] right-[5vw] object-contain"  src={camera} alt="camera"/>
-                    <img className="absolute w-[17vw] top-[5vh] left-[40vw] object-contain" src={stars} alt="stars"/>
-                    <img className="absolute w-[15vw] bottom-[5vh] left-[8vw] object-contain" src={nyota} alt="nyota"/>
-                    <img className="absolute w-[10vw] top-[40vh] left-[20vw] object-contain" src={bow} alt="bow"/>
-                    <img className="absolute w-[8vw] bottom-[5vh] left-[40vw] object-contain" src={ghibli} alt="ghibli"/>
-                    <img className="absolute w-[8vw] top-[40vh] right-[23vw] object-contain" src={coke} alt="coke"/>\
-                    <img className="absolute w-[15vw] bottom-[5vh] right-[5vw] object-contain" src={apple} alt="apple"/>
+                    <img className="absolute w-[15vw] top-[5vh] left-[5vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={cake} alt="cake"/>
+                    <img className="absolute w-[17vw] top-[5vh] right-[5vw] object-contain animate-[bounce_4s_infinite_ease-in-out]"  src={camera} alt="camera"/>
+                    <img className="absolute w-[17vw] top-[5vh] left-[40vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={stars} alt="stars"/>
+                    <img className="absolute w-[15vw] bottom-[5vh] left-[8vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={nyota} alt="nyota"/>
+                    <img className="absolute w-[10vw] top-[40vh] left-[20vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={bow} alt="bow"/>
+                    <img className="absolute w-[8vw] bottom-[5vh] left-[40vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={ghibli} alt="ghibli"/>
+                    <img className="absolute w-[8vw] top-[40vh] right-[23vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={coke} alt="coke"/>\
+                    <img className="absolute w-[15vw] bottom-[5vh] right-[5vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={apple} alt="apple"/>
                 </div>
                 <div>
                     <h1 className="absolute inset-0 flex items-center justify-center text-[15vw] font-cursive text-[#74A662]">Joy</h1>
