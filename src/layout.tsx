6cb387f8980f4f2c6fcca94328497479ce3ccd47
Joy <xiaominzhu@gmail.com>
2025-08-12 23:57:07 +1200

    refinement to mobile view

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,27 +1,75 @@
+import {useState} from "react";
 import {Link, Outlet} from "react-router";
 import {ModeToggle} from "@/components/ui/mode-toggle.tsx";
 import pig from "./assets/HomePageImages/Pig.png";
+import {Squash as Hamburger} from "hamburger-react";
 
 function NavBar() {
+    const [isOpen, setOpen] = useState(false);
+
     return (
-        <div className="w-full bg-background/80 backdrop-blur px-4 py-2 flex items-center justify-between border-b ">
-            <Link to={"/"}>
-                <img className="w-20 m-2" src={pig} alt="Home"/>
-            </Link>
-            <Link to={"/about"}>
-                <span className="text-3xl font-semibold font-serif">About</span>
-            </Link>
-            <Link to={"/Projects"}>
-                <span className="text-3xl font-semibold font-serif">Projects</span>
-            </Link>
-            <Link to={"/Leadership"}>
-                <span className="text-3xl font-semibold font-serif">Leadership</span>
-            </Link>
-            <Link to={"/Gallery"}>
-                <span className="text-3xl font-semibold font-serif">Gallery</span>
-            </Link>
-            <ModeToggle/>
-        </div>
+        <>
+            <div
+                className="w-full bg-background/80 backdrop-blur px-4 py-2 flex items-center justify-between border-b relative">
+                {/* Logo */}
+                <Link to={"/"}>
+                    <img className="w-16" src={pig} alt="Home"/>
+                </Link>
+
+                {/* Desktop links */}
+                <div className="hidden md:flex gap-6 items-center flex-1 justify-around ">
+                    <Link to={"/about"} className="text-3xl font-semibold font-serif">
+                        About
+                    </Link>
+                    <Link to={"/Projects"} className="text-3xl font-semibold font-serif">
+                        Projects
+                    </Link>
+                    <Link to={"/Leadership"} className="text-3xl font-semibold font-serif">
+                        Leadership
+                    </Link>
+                    <Link to={"/Gallery"} className="text-3xl font-semibold font-serif">
+                        Gallery
+                    </Link>
+
+                </div>
+
+                <div className="hidden md:block z-50">
+                    <ModeToggle/>
+                </div>
+
+                {/* Mobile hamburger */}
+                <div className="md:hidden">
+                    <Hamburger toggled={isOpen} toggle={setOpen}/>
+                </div>
+            </div>
+            {/* Right-side popout menu */}
+            <div
+                className={`fixed bg-secondary top-0 right-0 h-full w-64 border-l shadow-lg transform transition-transform duration-300 ease-in-out ${
+                    isOpen ? "translate-x-0" : "translate-x-full"
+                }`}
+            >
+                {/* Mobile hamburger */}
+                <div className="md:hidden">
+                    <Hamburger toggled={isOpen} toggle={setOpen}/>
+                </div>
+
+                <div className="p-6 flex flex-col gap-6 text-lg font-serif">
+                    <Link to={"/about"} onClick={() => setOpen(false)}>
+                        About
+                    </Link>
+                    <Link to={"/Projects"} onClick={() => setOpen(false)}>
+                        Projects
+                    </Link>
+                    <Link to={"/Leadership"} onClick={() => setOpen(false)}>
+                        Leadership
+                    </Link>
+                    <Link to={"/Gallery"} onClick={() => setOpen(false)}>
+                        Gallery
+                    </Link>
+                    <ModeToggle/>
+                </div>
+            </div>
+        </>
     );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,22 +10,67 @@ import stars from "@/assets/HomePageImages/stars.png";
 export default function IndexPage() {
     return (
         <>
-            <div className="relative w-full h-screen">
-                <div>
-                    <img className="absolute w-[17vw]  top-[5vh] left-[5vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={cake} alt="cake"/>
-                    <img className="absolute w-[17vw] top-[5vh] right-[5vw] object-contain animate-[bounce_4s_infinite_ease-in-out]"  src={camera} alt="camera"/>
-                    <img className="absolute w-[17vw] top-[5vh] left-[40vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={stars} alt="stars"/>
-                    <img className="absolute w-[15vw] bottom-[5vh] left-[8vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={nyota} alt="nyota"/>
-                    <img className="absolute w-[10vw] top-[40vh] left-[20vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={bow} alt="bow"/>
-                    <img className="absolute w-[8vw] bottom-[5vh] left-[40vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={ghibli} alt="ghibli"/>
-                    <img className="absolute w-[8vw] top-[40vh] right-[23vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={coke} alt="coke"/>
-                    <img className="absolute w-[15vw] bottom-[5vh] right-[5vw] object-contain animate-[bounce_4s_infinite_ease-in-out]" src={apple} alt="apple"/>
+            <div className="flex flex-col gap-10">
+                <div className="relative -z-5">
+                    <div className="relative w-full md:h-screen h-[600px]">
+                        <img
+                            className="absolute w-[25.5vw] md:w-[17vw] top-[5%] left-[5%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={cake} alt="cake"/>
+                        <img
+                            className="absolute w-[25.5vw] md:w-[17vw] top-[5%] right-[5%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={camera} alt="camera"/>
+                        <img
+                            className="absolute w-[25.5vw] md:w-[17vw] top-[5%] left-[40%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={stars} alt="stars"/>
+                        <img
+                            className="absolute w-[22.5vw] md:w-[15vw] bottom-[5%] left-[8%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={nyota} alt="nyota"/>
+                        <img
+                            className="absolute w-[20vw] md:w-[10vw] top-[30%] left-[20%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={bow} alt="bow"/>
+                        <img
+                            className="absolute w-[18vw] md:w-[8vw] bottom-[5%] left-[40%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={ghibli} alt="ghibli"/>
+                        <img
+                            className="absolute w-[12vw] md:w-[8vw] top-[30%] right-[23%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={coke} alt="coke"/>
+                        <img
+                            className="absolute w-[22.5vw] md:w-[15vw] bottom-[5%] right-[5%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={apple} alt="apple"/>
+                    </div>
                 </div>
-                <div>
-                    <h1 className="absolute inset-0 flex items-center justify-center text-[200px] sm:text-[400px] font-heading text-[#74A662]">Joy</h1>
+                <div className="relative -z-5">
+                    <div className="relative w-full md:h-screen h-[600px] block md:hidden">
+                        <img
+                            className="absolute w-[25.5vw] md:w-[17vw] top-[5%] left-[5%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={cake} alt="cake"/>
+                        <img
+                            className="absolute w-[25.5vw] md:w-[17vw] top-[5%] right-[5%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={camera} alt="camera"/>
+                        <img
+                            className="absolute w-[25.5vw] md:w-[17vw] top-[5%] left-[40%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={stars} alt="stars"/>
+                        <img
+                            className="absolute w-[22.5vw] md:w-[15vw] bottom-[5%] left-[8%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={nyota} alt="nyota"/>
+                        <img
+                            className="absolute w-[15vw] md:w-[10vw] top-[40%] left-[20%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={bow} alt="bow"/>
+                        <img
+                            className="absolute w-[12vw] md:w-[8vw] bottom-[5%] left-[40%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={ghibli} alt="ghibli"/>
+                        <img
+                            className="absolute w-[12vw] md:w-[8vw] top-[40%] right-[23%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={coke} alt="coke"/>
+                        <img
+                            className="absolute w-[22.5vw] md:w-[15vw] bottom-[5%] right-[5%] object-contain animate-[bounce_4s_infinite_ease-in-out]"
+                            src={apple} alt="apple"/>
+                    </div>
                 </div>
             </div>
+            <div>
+                <h1 className="absolute inset-0 flex items-center justify-center text-[200px] sm:text-[400px] font-heading text-[#74A662]">Joy</h1>
+            </div>
         </>
-
     )
 }
